6a5a042df17fa7b268510d2f3e4fd81a596cd805
BANDARIMANOJKUMAR359 <manojbandari359@gmail.com>
2025-08-03 23:47:52 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from app import app
 from waitress import serve
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=5000)
-
