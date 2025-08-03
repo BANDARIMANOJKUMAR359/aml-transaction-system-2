ab75da4bf5da678e232efcbaebce782aed9729de
BANDARIMANOJKUMAR359 <manojbandari359@gmail.com>
2025-08-04 02:18:24 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,10 +1,21 @@
+import os
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'super-secret-key'
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # A default secret that should be overridden with a random value
+        SECRET_KEY='dev',
+    )
 
-    with app.app_context():
-        from . import routes
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import routes
+    app.register_blueprint(routes.main)
 
     return app
